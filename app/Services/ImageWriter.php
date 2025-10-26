eacef007a11c17a0e7dc40362fa39c5c82704602
DDEV User <nobody@example.com>
2025-10-26 16:11:43 +0100

    feat: always save original image
    Very hacky. But with this version I do
    not have to change a lot of existing
    code, so future merges with origin will
    be easier.
    Save the original version of the image
    for future updates in the highes
    possible resolution.
    Still re-encode for security

--- app/Services/ImageWriter.php
+++ app/Services/ImageWriter.php
@@ -3,9 +3,11 @@
 namespace App\Services;
 
 use App\Values\ImageWritingConfig;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Str;
 use Intervention\Image\FileExtension;
+use Intervention\Image\Interfaces\ImageInterface;
 use Intervention\Image\Interfaces\ImageManagerInterface;
 use Intervention\Image\Laravel\Facades\Image;
 use RuntimeException;
@@ -36,15 +38,51 @@ class ImageWriter
 
     public function write(string $destination, mixed $source, ?ImageWritingConfig $config = null): void
     {
+        $img = Image::read(Str::isUrl($source) ? Http::get($source)->body() : $source);
+
+        if ($config instanceof ImageWritingConfig !== true) {
+            $this->saveOriginalImage($destination, $img);
+        }
+
         $config ??= ImageWritingConfig::default();
 
-        $img = Image::read(Str::isUrl($source) ? Http::get($source)->body() : $source)
-            ->scale(width: $config->maxWidth);
+        $img->scale(width: $config->maxWidth);
 
         if ($config->blur) {
             $img->blur($config->blur);
         }
 
         $img->save($destination, $config->quality, $this->extension);
+    }
+
+    /**
+     * Very hacky. But with this version I do not have to change a lot of existing code,
+     * so future merges with origin will be easier.
+     * Save the original version of the image for future updates in the highes possible
+     * resolution. Still re-encode for security
+     */
+    private function saveOriginalImage(string $destination, ImageInterface $img): void
+    {
+        $origin = $img->origin();
+
+        $originalExtension = $origin->fileExtension()
+            ? FileExtension::tryCreate($origin->fileExtension())
+            : null;
+
+        if ($originalExtension === null) {
+            $originalExtension = FileExtension::tryCreate($origin->mediaType());
+        }
+
+        $fileExtension = $originalExtension->value ?? $this->extension->value;
+
+        $originalImagePath = image_storage_path(sprintf(
+            '%s_org.%s',
+            Str::beforeLast(basename($destination), '.'),
+            $fileExtension
+        ));
+
+        if (File::exists($originalImagePath) !== true) {
+            $img->save($originalImagePath, quality: 90);
+        }
     }
 }
